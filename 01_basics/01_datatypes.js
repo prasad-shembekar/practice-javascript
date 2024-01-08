844a07d718382864112d4d3d168db4023f3336ab
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-08 06:26:55 +0000

    Update

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code as newer Version 
+
+// alert("Hello");
+// alert(3+3); we are using node js not browser
+
+console.log(3+3); 
+console.log("Prasad");
+// https://tc39.es/ecma262/
+
+let name = "prasad";
+let age = 24;
+let isLoggedIn =false;
+let state = null; //khali hai  0 temp na lete huye null lena 
+let state1; //undefined abhi value assign nahi hui hai
+
+
+// number => 2 to power 33
+// bigInt
+// string => "" / ''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
+
