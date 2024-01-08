7f4f49ffce269c58db6b10b6cc202cb9984981a4
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-08 06:40:02 +0000

    Update

---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,10 @@
+// let score = "33abc";
+// let score = null; //to number is 0
+let score = undefined; // NaN
+console.log(typeof score);
+console.log(typeof(score));
+
+let new_score = Number(score);
+console.log(typeof new_score);
+console.log(new_score);
+
