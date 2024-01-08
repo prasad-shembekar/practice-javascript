d1c170a1bdfdd4031f2de46969dc3fd54e958e9e
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-08 06:45:37 +0000

    Update

--- 01_basics/01_conversion.js
+++ 01_basics/01_conversion.js
@@ -1,6 +1,8 @@
 // let score = "33abc";
 // let score = null; //to number is 0
-let score = undefined; // NaN
+// let score = undefined; // NaN
+let score = true;
+
 console.log(typeof score);
 console.log(typeof(score));
 
@@ -8,3 +10,14 @@ let new_score = Number(score);
 console.log(typeof new_score);
 console.log(new_score);
 
+// "33" => 33
+// "33abc" = > NaN
+// true => 1; false => 0;
+
+// let isLoggedIn = 1;
+// let isLoggedIn = ""; //false
+// let isLoggedIn = "Prasad"; //true
+let isLoggedIn = " "; //true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
