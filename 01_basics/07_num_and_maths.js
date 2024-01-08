458d8e1cb0c4b044e1de956e00cf1f3fa4024cff
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-08 14:24:16 +0000

    Update

---
+++ 01_basics/07_num_and_maths.js
@@ -0,0 +1,10 @@
+const score = 400;
+console.log(score);
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNum = 23.8966;
+console.log(otherNum.toPrecision());
