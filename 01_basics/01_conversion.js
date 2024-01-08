95f3925dd0db8b974e3fd55cc7a3c8de75d6611a
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-08 07:24:55 +0000

    Update

--- 01_basics/01_conversion.js
+++ 01_basics/01_conversion.js
@@ -3,12 +3,12 @@
 // let score = undefined; // NaN
 let score = true;
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let new_score = Number(score);
-console.log(typeof new_score);
-console.log(new_score);
+// console.log(typeof new_score);
+// console.log(new_score);
 
 // "33" => 33
 // "33abc" = > NaN
@@ -22,7 +22,7 @@ console.log(new_score);
 let isLoggedIn = " "; //true
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //  1 => true; 0=> false
 //  "" => false; " " => true
@@ -30,4 +30,43 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let strNumber = String(someNumber);
-console.log(typeof strNumber);
+// console.log(typeof strNumber);
+
+
+// ********** Operations *****************
+
+let value  = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1  = "hello";
+let str2 = " prasad";
+let str3 = str1 + str2;
+console.log(str3);
+
+//left to right assignment
+console.log("1" + 2); //12
+console.log(1 + 5 + "2"); //62
+console.log("1" + "2");
+console.log("1"+2+2);
+console.log(2+2 + "1"+2); //412
+
+console.log(+true); //1
+console.log(+""); //0
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+// gameCounter++; //101
+++gameCounter; //101
+console.log(gameCounter);
+
