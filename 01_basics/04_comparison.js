5a2b94d0910396f1a59ce74e9cd390bbf53d7032
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-08 09:05:49 +0000

    Update

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -11,3 +11,11 @@ console.log("02" > 1); //true
 console.log(null >0); //false
 console.log(null == 0); //false
 console.log(null >= 0); //true
+
+console.log(undefined == 0); //false
+console.log(undefined === 0); //false
+
+console.log("2" === 2); //false
+
+
+
