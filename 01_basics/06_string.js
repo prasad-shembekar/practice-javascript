ffeebfafb334b9b11ba2a73a7f8dc313072f6928
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-08 14:09:59 +0000

    Update

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -6,7 +6,7 @@ const repoCount = 50;
 console.log(`Hello My Name is ${name} and My repos are ${repoCount}`);
 
 
-const gameName = new String("Prasad Octa");
+const gameName = new String("Prasad-Octa");
 console.log(gameName);
 console.log(gameName[0]);
 console.log(gameName.__proto__);
@@ -14,5 +14,22 @@ console.log(gameName.__proto__);
 console.log(gameName.length);
 console.log(gameName.toUpperCase()); //not changed original string
 console.log(gameName.charAt(2));
+console.log(gameName.indexOf('O'));
 
+const newString = gameName.substring(0,4); //sibstring do not take negative value
+console.log(newString);
+
+const anotherstr = gameName.slice(-8,4);
+console.log(anotherstr); //s
+
+const newString1 = "   Hii ";
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://prasad.com/prasad%20shembekar";
+console.log(url.replace('%20','-'))
+console.log(url.includes('prasad'));
+
+const strArr = new String("prasad-madhukar-shembekar");
+console.log(strArr.split('-'));
 
