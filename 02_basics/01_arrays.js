cc7f458b64a8d696e54a467bfe1868d5aa5a3b91
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-09 10:19:03 +0000

    Update

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,3 +2,15 @@
 
 const myArr = [0,1,2,3,4,5,true]; // heterogenous and resizable
 
+const myHeros = ["Shaktiman","Nagraj"];
+
+const myArr2 = new Array(1,2,3,4);
+console.log(myArr2[0]);
+
+// Array Methods
+
+myArr.push(6);
+myArr.push("Value");
+myArr.pop();
+console.log(myArr);
+myArr.unshift("Add new value");
