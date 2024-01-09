3207ca2015b1da28641a83f44e97d51f05660216
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-09 09:18:31 +0000

    Update

--- 01_basics/07_num_and_maths.js
+++ 01_basics/07_num_and_maths.js
@@ -7,4 +7,21 @@ console.log(balance.toString().length);
 console.log(balance.toFixed(1));
 
 const otherNum = 23.8966;
-console.log(otherNum.toPrecision());
+console.log(otherNum.toPrecision(3));
+
+const hundreads = 1000000;
+console.log(hundreads.toLocaleString('en-IN'));
+
+
+// -----------------Maths------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //4
+// console.log(Math.round(4.3)); //4
+// console.log(Math.round(4.6)); //5
+// console.log(Math.ceil(4.2)); //5
+// console.log(Math.floor(4.2)); //4
+// console.log(Math.min(4,3,5,6)); //3
+// console.log(Math.max(4,3,5,6)); //6
+
+console.log(Math.random()); //always between 0 and 1
