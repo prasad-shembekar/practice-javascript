50ea0b33a99c04a99c6744608954212a5b54b32a
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-13 09:41:30 +0000

    Update

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -1,11 +1,11 @@
 (function chai(){
     console.log(`DB connected...`); 
-})()
+})();
 
 // ()(); first is function defination and second is execution
 
-(function aur(){
-    console.log("DB")
-} )();
+(()=>{
+    console.log("DB");
+})();
 
 
