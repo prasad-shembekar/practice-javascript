2982dbc68207aecca15b5edc4f874d43d3f0cfc9
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-13 11:15:12 +0000

    Update

---
+++ 04_control_flow/one.js
@@ -0,0 +1,30 @@
+// if
+const isUserLoggedIn = true;
+
+if(isUserLoggedIn){
+
+}
+// <,>,<=,>=,
+
+// = assignment
+// == comparison 
+// === strict comparision 
+
+
+if(2==2){
+    console.log("It is true");
+}
+
+if(false){
+
+}
+
+if(2=='2'){
+    console.log("true");
+}
+if(2==='2'){
+    console.log('true')
+}else{
+    console.log("false");
+}
+
