fcb1db39d4cce76775b93ea71c35d64640833969
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-13 12:52:40 +0000

    Update

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -2,3 +2,21 @@
     // break;
 
 const month = 3;
+
+switch (month) {
+    case 1:
+        console.log('January');
+        break;
+    case 1:
+        console.log('January');
+        break;
+    case 1:
+        console.log('January');
+        break;
+    case 1:
+        console.log('January');
+        break;
+
+    default:
+        break;
+}
