d5a819e4d2bc208baaf839b0ade71adcebed1c19
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-15 12:27:53 +0000

    Update

--- 05_Iterations/seven.js
+++ 05_Iterations/seven.js
@@ -10,8 +10,10 @@ console.log(happy);
 // chaining method
 const myNumbers1 = [1,2,3,4,5,6,7,8,9,10];
 const happy1 = myNumbers1
-                .map((num)=>num*10) //10,20,30,40,50,
-                .map((num)=>num +1); //11,21,31,41,51
+                .map((num)=>num*10) 
+                .map((num)=>num +1) 
+                .filter((num)=>num >= 40);
+
 console.log(happy1);
 
 
