4d94626ec72c8caca63c4c5410f889cd5e886670
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-15 12:13:01 +0000

    Update

--- 05_Iterations/seven.js
+++ 05_Iterations/seven.js
@@ -3,4 +3,7 @@ const myNumbers = [1,2,3,4,5,6,7,8,9,10];
 const newNums = myNumbers.map((num)=> num+10);
 console.log(newNums);
 
-newNums.for
+const happy = [];
+newNums.forEach((item)=>{ happy.push(item+10)});
+console.log(happy);
+
