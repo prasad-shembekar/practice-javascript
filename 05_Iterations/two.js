8b59467a26f5f143c4227f4d3494240c5be7a464
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-15 09:28:11 +0000

    Update

---
+++ 05_Iterations/two.js
@@ -0,0 +1,20 @@
+let i = 0;
+while (i<=10) {
+    console.log("HIiiiiiiiii");
+    i++;
+}
+
+let myArr = ['flash','superman','einestine']
+
+let arr = 0;
+while(arr<myArr.length){
+    console.log(`Value is ${myArr[arr]}`);
+    arr = arr + 1;
+}
+
+
+
+
+
+
+
