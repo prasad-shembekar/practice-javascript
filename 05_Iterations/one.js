dd7adc7c99741f0421814a875badf1d147c3c152
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-15 09:11:51 +0000

    Update

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -10,12 +10,17 @@ for (let i = 0; i <= 10; i++) {
 // console.log(index); due to block scope we can not access it 
 // first index gets initialised then we compare its value if true then code gets executed then index increament done again value checked then code executes
 
-for (let i = 0; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
     console.log(`It is table of ${i}:`);
     for (let j = 0; j <= 10; j++) {
         console.log(i + '*' + j + '=' +i*j);
     }
 }
 
+let myArr = ["flash",'superman','batman'];
 
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+    console.log(element);
+}
 
