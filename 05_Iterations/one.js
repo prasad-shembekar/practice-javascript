f4eba6cfb7e7bc3131231e76c7b7af5a01141c62
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-15 09:06:19 +0000

    Update

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -1,8 +1,19 @@
 // for loop
 
-for (let index = 0; index <= 10; index++) {
-    const element = index;
-    console.log(index);
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if(element == 5){
+        // console.log("5 is best number");
+    }
+    // console.log(i);
+}
+// console.log(index); due to block scope we can not access it 
+// first index gets initialised then we compare its value if true then code gets executed then index increament done again value checked then code executes
+
+for (let i = 0; i <= 10; i++) {
+    console.log("It is val of I: ",i);
+    for (let j = 0; j <= 10; j++) {
+        console.log("J =", j);
+    }
 }
 
-// console.log(index); due to block scope we can not access it 
