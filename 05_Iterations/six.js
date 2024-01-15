44c16b2a16f73ad283355956207dfaa626057749
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-15 11:46:51 +0000

    Update

---
+++ 05_Iterations/six.js
@@ -0,0 +1,14 @@
+const coding = ['js','py','java','cpp'];
+
+const values = coding.forEach((item)=>{
+    // console.log(item);
+    return item; //you will have to say we are returning 
+})
+
+// console.log(values);
+
+
+const myNum = [1,2,3,4,5,6,7,8,9,10];
+const val = myNum.filter((num)=> num>4)
+
+console.log(val);
