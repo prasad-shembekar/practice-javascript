24cfae16a92b6c22cc8f34dd8e7feed368a20ac1
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-15 10:45:31 +0000

    Update

--- 05_Iterations/four.js
+++ 05_Iterations/four.js
@@ -6,5 +6,12 @@ const myObj1 = {
 }
 
 for (const key in myObj1) {
-    console.log(key)
+    console.log(myObj1[key]);
+}
+
+const lang = ['js','rb','py','cpp'];
+
+for (const key in lang) {
+    console.log(key);
+    console.log(lang[key]);
 }
