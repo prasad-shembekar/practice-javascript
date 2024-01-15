579d366703ff1c0a661117ce97e79b1a246563f0
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-15 12:33:47 +0000

    Update

--- 05_Iterations/eight.js
+++ 05_Iterations/eight.js
@@ -8,3 +8,23 @@ const myNums = [1,2,3];
 
 const myTotal = myNums.reduce((acc,curVal)=> acc + curVal,0);
 console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: 'js course',
+        price: 2999
+    },
+    {
+        itemName: 'mobile course',
+        price: 2999
+    },
+    {
+        itemName: 'Data science course',
+        price: 15999
+    },
+]
+
+console.log(shoppingCart);
+
+const pricepay = shoppingCart.reduce((acc,item)=> acc + item.price,0)
+console.log(pricepay);
