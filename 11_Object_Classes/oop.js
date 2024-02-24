41dc32274609d48eb3e1489af82fdab072def0c3
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-02-24 08:16:18 +0000

    Update

--- 11_Object_Classes/oop.js
+++ 11_Object_Classes/oop.js
@@ -3,10 +3,11 @@ const user = {
     loginCount: 8,
     signedIn: true,
     getUserDetails: function(){
-        console.log("Got user details from database...")
+        // console.log("Got user details from database...");
+        console.log(`Username: ${username}`);
     }
 }
-
+// this keyword deals with current context
 console.log(user.username);
 console.log(user.getUserDetails());
 
