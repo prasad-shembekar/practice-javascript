3d9040d47daeed0c76c4b55c0d100500fb50d386
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-02-24 06:05:18 +0000

    Update

--- 10_advance_one/promises.js
+++ 10_advance_one/promises.js
@@ -1,4 +1,29 @@
-// Promise Object represents eventual completion or failure
+// Promise is a Object represents eventual completion or failure
 // Kernal ko request ke Baad file milegi
 // Generally we consume promises 
 
+// fetch('www.something.com').then().catch(),finally();
+//  Q or BlueBird were commonly used when we dont had promise object in javascript
+
+const promiseOne = new Promise(function(resolve,reject){
+    // Do an async task
+    // DB calls, Cryptography, Networking call
+
+    setTimeout(function(){
+        resolve();
+        console.log('Async Task is completed'); //pahle task complete hoga badme promise chalega 
+    },1000);
+});
+
+promiseOne.then(function(){
+    console.log("Promise Consumed...");
+})
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve();
+        console.log("Async task 2");
+    },1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
