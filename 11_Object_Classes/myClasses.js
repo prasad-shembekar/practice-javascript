36450dfaac75cdb91682772f4cbbcdc83f83ff19
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-03-05 14:22:50 +0000

    Update

---
+++ 11_Object_Classes/myClasses.js
@@ -0,0 +1,41 @@
+// ES6
+
+// class User{
+//     constructor(username,email,password){
+//         this.username  = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+
+//     changeUser(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const chai = new User("chai","chai@gmail.com","123");
+// console.log(chai.encryptPassword())
+// console.log(chai.changeUser());
+
+// behind the scene
+
+function User(username,email,password){
+    this.username  = username;
+        this.email = email;
+        this.password = password;
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+
+User.prototype.changeUser = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const Tea = new User('tea',"tea@gmail.com","123");
+console.log(Tea.encryptPassword());
+console.log(Tea.changeUser());
