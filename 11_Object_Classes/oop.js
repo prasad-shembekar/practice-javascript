ac31d3011d09a4e439350057b22e27821a7296c1
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-03-05 12:36:24 +0000

    Update

--- 11_Object_Classes/oop.js
+++ 11_Object_Classes/oop.js
@@ -40,13 +40,39 @@ console.log(u2);
 // arrow function me this ka access nahi hota quki prototype usme aa jata hai
 
 function multipleBy5(num){
+    this.num = num;
     return num*5;
 }
 
 multipleBy5.power = 2;
 
-console.log(multipleBy5.power);
-console.log(multipleBy5.prototype);
+console.log(multipleBy5.power); //2
+console.log(multipleBy5.prototype); //{}
+
+function createUser(username,score){
+    this.username = username;
+    this.score = score;
+}
+
+createUser.prototype.increment = function(){
+    this.score++; //this matlab jis so jisne bhi bulaya hai uska kaam krdo
+}
+
+// const us1  = createUser("us1",25); //it wont work it do not have new keyword  and will throw cant read properties of undefined ayega
+const us2 =  new createUser('us2',250);
+
+createUser.prototype.printMe = function(){
+    console.log(`score is ${this.score}`);
+}
+
+// myArray.prototype.map()
+
+us2.printMe();
+
+
+
+
+
 
 
 
---
+++ 11_Object_Classes/prototype.js
@@ -0,0 +1,21 @@
+// let myName = "Prasad"
+// let myChannel = "   chai   ";
+// console.log(myName.length)
+// console.log(myName.trim().length);
+// console.log(myName.trueLength);
+
+let myHeros  = ['thor','spiderman']
+let heroPower = {
+    thor: "hammer",
+    spiderman: "spider web",
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+
+
+Object.prototype.prasad = function(){
+    console.log("prasad is present in all objects ")
+}
+heroPower.prasad();
